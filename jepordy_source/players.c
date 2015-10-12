d9a6649087b2542674cea589b92bdd3a09ca2f94
davidm4000 <davidm4000@gmail.com>
2015-10-12 12:34:50 -0700

    Functions player_exists and update_score added in players.c, NUM_PLAYERS added in players.h

--- jepordy_source/players.c
+++ jepordy_source/players.c
@@ -10,14 +10,54 @@
 #include <string.h>
 #include "players.h"
 
-// Returns true if the player name matches one of the existing players
+// Function that checks if a player exists in the players array.
 bool player_exists(player *players, char *name)
 {
-    return false;
+	// Variable remains false unless player is found
+	bool playerExists = false;
+
+	// Loop through the players array comparing each name to *name
+	for(int i = 0; i < NUM_PLAYERS; i++)
+	{
+		// Check if player name exists
+	     	if(strcmp(name, players[i].name)==0)
+	     	{
+	     		playerExists = true; // The player was found.
+	     	}
+	}
+
+	// Return the boolean holding of the player exists or not
+    	return playerExists;
 }
 
-// Updates the score for that player given their name
+// Function that checks if a player exists then updates their score if true.
 void update_score(player *players, char *name, int score)
 {
-    
+	// Index that holds which index belongs to the player
+    	int playerIndex = -1;
+	
+	// Loop through players
+	for(int i = 0; i < NUM_PLAYERS; i++)
+	{
+		// Check if player name exists
+		if(strcmp(name, players[i].name)==0)
+	     	{
+			// Set player index to the player to change score.
+	     		playerIndex = i;
+	     	}
+	}
+	
+	// If playerIndex wasn't changed - Player not found
+	if(playerIndex == -1)
+	{
+		// Display the player was not found.
+		printf("\nError: Player not found\n");
+	}
+	else // Player was found
+	{
+	// Player was found, update the score.
+	players[playerIndex].score += score;
+	}
 }
+
+
